4717a87b3fa67cf785e759be98a25260fb90d9c8
David Wekesa <davidwekesar@gmail.com>
2022-03-24 12:17:38 +0300

    Remove unused resources

--- app/src/main/java/com/example/weatherapp/database/LocationsDatabase.kt
+++ app/src/main/java/com/example/weatherapp/database/LocationsDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [DatabaseLocation::class], version = 1)
+@Database(entities = [DatabaseLocation::class], version = 1, exportSchema = false)
 abstract class LocationsDatabase: RoomDatabase() {
     abstract val locationDao: LocationDao
 }
