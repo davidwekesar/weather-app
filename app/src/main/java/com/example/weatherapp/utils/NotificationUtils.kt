2bd0f1aa5f72e0c1b2938d173c52157e4f19fe90
David Wekesa <davidwekesar@gmail.com>
2022-04-05 10:43:46 +0300

    Feat: click notification to navigate to app

--- app/src/main/java/com/example/weatherapp/utils/NotificationUtils.kt
+++ app/src/main/java/com/example/weatherapp/utils/NotificationUtils.kt
@@ -1,14 +1,35 @@
 package com.example.weatherapp.utils
 
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.os.Build
 import androidx.core.app.NotificationCompat
 import com.example.weatherapp.R
 import com.example.weatherapp.domain.Location
+import com.example.weatherapp.ui.MainActivity
 
 private const val NOTIFICATION_ID = 0
 
 fun NotificationManager.sendNotification(location: Location, appContext: Context) {
+    val contentIntent = Intent(appContext, MainActivity::class.java)
+    val contentPendingIntent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        PendingIntent.getActivity(
+            appContext,
+            NOTIFICATION_ID,
+            contentIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+    } else {
+        PendingIntent.getActivity(
+            appContext,
+            NOTIFICATION_ID,
+            contentIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+    }
     val builder = NotificationCompat
         .Builder(appContext, appContext.getString(R.string.weather_notification_channel_id))
         .setSmallIcon(R.mipmap.ic_launcher_round)
@@ -20,6 +41,8 @@ fun NotificationManager.sendNotification(location: Location, appContext: Context
             )
         )
         .setContentText(location.weatherText)
+        .setContentIntent(contentPendingIntent)
+        .setPriority(NotificationCompat.PRIORITY_LOW)
         .setAutoCancel(true)
 
     notify(NOTIFICATION_ID, builder.build())
